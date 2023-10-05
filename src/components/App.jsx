73946417620167bcf927750e1f7de33295b072c8
domanskayelyzaveta <domanskaya_@ukr.net>
2023-10-05 20:57:12 +0200

    hw refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Feedback from './Feedbacks/Feedback';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 import '../components/App.css';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = state => {
+    switch (state) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
-  onLeaveFeedback = state => {
-    this.setState(prevState => ({ [state]: prevState[state] + 1 }));
-  };
+  const total = good + neutral + bad;
+  const positive = Math.round(total !== 0 ? (good / total) * 100 : 0);
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-    const total = good + neutral + bad;
-    const positive = Math.round(total !== 0 ? (good / total) * 100 : 0);
+  const options = ['good', 'neutral', 'bad'];
 
-    return (
-      <div>
-        <div className="container">
-          <Section title="Please leave your feedback">
-            <Feedback
-              options={options}
-              onLeaveFeedback={this.onLeaveFeedback}
+  return (
+    <div>
+      <div className="container">
+        <Section title="Please leave your feedback">
+          <Feedback options={options} onLeaveFeedback={onLeaveFeedback} />
+        </Section>
+
+        <Section title="Statistics">
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positive={positive}
             />
-          </Section>
-
-          <Section title="Statistics">
-            {total ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={total}
-                positive={positive}
-              />
-            ) : (
-              <Notification message="There is no feedback" />
-            )}
-          </Section>
-        </div>
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
